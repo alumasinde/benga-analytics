6dd9baa932603805e93e8d6ba17ab22ef33176ae
Albert Masinde <143938688+alumasinde@users.noreply.github.com>
2026-09-07 18:56:42 +0300

    fix(app): correct database initialization syntax

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ from werkzeug.utils import secure_filename
 from pymongo.errors import DuplicateKeyError
 from bson import ObjectId
 from dotenv import load_dotenv
-import database\n\ndb = database.init_database()
+import database
+
+db = database.init_database()
 from analyzer import read_dataframe, infer_and_clean, dataframe_records, build_insights
 load_dotenv()
 app = Flask(__name__, static_folder="static", template_folder="templates")
